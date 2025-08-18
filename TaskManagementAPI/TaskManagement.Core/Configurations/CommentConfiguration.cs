f59dd58c26b1c314f80480335b4337b7949be8e9
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-08-18 20:04:48 +0300

    Implement project configuration

--- TaskManagementAPI/TaskManagement.Core/Configurations/CommentConfiguration.cs
+++ TaskManagementAPI/TaskManagement.Core/Configurations/CommentConfiguration.cs
@@ -1,6 +1,14 @@
-﻿namespace TaskManagementAPI.TaskManagement.Core.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagementAPI.TaskManagement.Core.Entities;
+
+namespace TaskManagementAPI.TaskManagement.Core.Configurations
 {
-    public class CommentConfiguration
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
     {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+
+        }
     }
 }
--- TaskManagementAPI/TaskManagement.Core/Configurations/ProjectConfiguration.cs
+++ TaskManagementAPI/TaskManagement.Core/Configurations/ProjectConfiguration.cs
@@ -1,6 +1,13 @@
-﻿namespace TaskManagementAPI.TaskManagement.Core.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagementAPI.TaskManagement.Core.Entities;
+
+namespace TaskManagementAPI.TaskManagement.Core.Configurations
 {
-    public class ProjectConfiguration
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
     {
+        public void Configure(EntityTypeBuilder<Project> builder) { 
+        
+        }
     }
 }
