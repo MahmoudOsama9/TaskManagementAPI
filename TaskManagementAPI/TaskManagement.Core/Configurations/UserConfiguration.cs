e144dd161c1d9310f646b9f9601ab66101c18307
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-08-20 19:08:03 +0300

    iteration

--- TaskManagementAPI/TaskManagement.Core/Configurations/UserConfiguration.cs
+++ TaskManagementAPI/TaskManagement.Core/Configurations/UserConfiguration.cs
@@ -1,6 +1,11 @@
-﻿namespace TaskManagementAPI.TaskManagement.Core.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagementAPI.TaskManagement.Core.Entities;
+
+namespace TaskManagementAPI.TaskManagement.Core.Configurations
 {
-    public class UserConfiguration
+    public class UserConfiguration : IEntityTypeConfiguration<User>
     {
+        public void Configure(EntityTypeBuilder<User> builder) { }
     }
 }
