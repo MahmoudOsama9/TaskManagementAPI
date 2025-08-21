29a8fadf9ff9e0e2eea0216c1405c09204dceb19
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-08-21 13:10:15 +0300

    config Comment

--- TaskManagementAPI/TaskManagement.Core/Configurations/CommentConfiguration.cs
+++ TaskManagementAPI/TaskManagement.Core/Configurations/CommentConfiguration.cs
@@ -8,7 +8,24 @@ namespace TaskManagementAPI.TaskManagement.Core.Configurations
     {
         public void Configure(EntityTypeBuilder<Comment> builder)
         {
+            builder.HasKey(x => x.Id);
 
+            builder.Property(c => c.Content)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            builder.Property(c => c.CreatedAt)
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.HasOne(c => c.TaskItem)
+                .WithMany(c => c.Comments)
+                .HasForeignKey(c => c.TaskItemId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(c => c.User)
+                .WithMany(c => c.Comments)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
