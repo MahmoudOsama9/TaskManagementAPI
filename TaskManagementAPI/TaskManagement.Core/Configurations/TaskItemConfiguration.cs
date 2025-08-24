3b0a0e07f51ac7297a903273286957616e3d0380
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-08-24 13:08:36 +0300

    config

--- TaskManagementAPI/TaskManagement.Core/Configurations/TaskItemConfiguration.cs
+++ TaskManagementAPI/TaskManagement.Core/Configurations/TaskItemConfiguration.cs
@@ -9,6 +9,35 @@ namespace TaskManagementAPI.TaskManagement.Core.Configurations
         public void Configure(EntityTypeBuilder<TaskItem> builder)
         {
             builder.ToTable("Tasks");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Title)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(t => t.Status)
+                .IsRequired();
+
+            builder.Property(t =>t.Description)
+                .HasMaxLength(2000);
+
+            builder.Property(t => t.Priority)
+                .IsRequired();
+
+            builder.HasOne(t => t.Project)
+                .WithMany(t => t.Tasks)
+                .HasForeignKey(t => t.Project.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(t => t.AssignedUser)
+                .WithMany(t => t.AssignedTasks)
+                .HasForeignKey(t => t.AssignedUserId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasMany(t => t.Comments)
+                .WithOne(t => t.TaskItem)
+                .HasForeignKey(t => t.TaskItemId);
         }
 
     }
